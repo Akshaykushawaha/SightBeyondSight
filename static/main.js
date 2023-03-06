729606f9d659a715ca1b0d65c10dbb15d1378006
Yash Dhanore <yashdhanore@gmail.com>
2023-03-06 15:27:05 +0100

    API key change

--- static/main.js
+++ static/main.js
@@ -106,3 +106,18 @@ function myFunction() {
   // Alert the copied text
   alert("Copied the text: " + copyText.value);
 }
+
+function myFunction1() {
+  // Get the text field
+  var copyText = document.getElementById("myInput-1");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
